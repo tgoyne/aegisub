34b14a804ddecbaca0531fbefbddc41963cbe0ab
archmagezeratul <archmagezeratul@93549f3f-7f0a-0410-a4b3-e966c9c94f04>
2007-04-14 01:02:21 +0000

    Early and mostly uncoded spellchecker dialog.
    git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@1075 93549f3f-7f0a-0410-a4b3-e966c9c94f04

--- aegisub/video_display_visual.cpp
+++ aegisub/video_display_visual.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2005-2007, Rodrigo Braz Monteiro
+﻿// Copyright (c) 2005-2007, Rodrigo Braz Monteiro
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
