1dbc490a4d1524b18b5ea1d09a0ed1a904775176
archmagezeratul <archmagezeratul@93549f3f-7f0a-0410-a4b3-e966c9c94f04>
2007-04-18 20:24:32 +0000

    A few Linux fixes
    git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@1095 93549f3f-7f0a-0410-a4b3-e966c9c94f04

--- aegisub/video_display_visual.cpp
+++ aegisub/video_display_visual.cpp
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2005-2007, Rodrigo Braz Monteiro
+// Copyright (c) 2005-2007, Rodrigo Braz Monteiro
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
