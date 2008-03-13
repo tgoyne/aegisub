11af079209ec1edd478f856b7444acbaec183a8c
Rodrigo Braz Monteiro <zeratul@cellosoft.com>
2008-03-13 21:28:22 +0000

    Renamed Aegilib to Gorgonsub.
    git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@2047 93549f3f-7f0a-0410-a4b3-e966c9c94f04

--- aegilib/include/aegilib/gorgonstring.h
+++ aegilib/include/aegilib/gorgonstring.h
@@ -27,7 +27,7 @@
 //
 // -----------------------------------------------------------------------------
 //
-// AEGISUB/AEGILIB
+// AEGISUB/GORGONSUB
 //
 // Website: http://www.aegisub.net
 // Contact: mailto:amz@aegisub.net
@@ -38,7 +38,7 @@
 #include <wx/string.h>
 #include <vector>
 
-namespace Aegilib {
+namespace Gorgonsub {
 
 	// Define the string type used throughout this library
 	//typedef std::basic_string<wchar_t> String;
