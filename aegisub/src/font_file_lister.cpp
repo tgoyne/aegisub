tgoyne/aegisub

commit ec83be6bf80d8eba1b34cd1e259dd98c45a90772

Remove 'pragma once' from colour_button.h font_file_lister.h gl_wrap.h subs_edit_ctrl.h.
git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@3536 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/font_file_lister.cpp
@@ -20,8 +20,7 @@
 // LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 // CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 // SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //
@@ -56,7 +55,6 @@
 #define FontListerClass FontConfigFontFileLister
 #endif
 
-#include "font_file_lister.h"
 #include "standard_paths.h"
 #include "text_file_reader.h"
 #include "text_file_writer.h"

aegisub/src/colour_button.h
@@ -35,9 +35,6 @@
 ///
 
 
-#pragma once
-
-
 ////////////
 // Includes
 #ifndef AGI_PRE

aegisub/src/dialog_options.cpp
@@ -59,7 +59,6 @@
 #include "options.h"
 #include "standard_paths.h"
 #include "subs_edit_box.h"
-#include "subs_edit_ctrl.h"
 #include "subs_grid.h"
 #include "subtitles_provider_manager.h"
 #include "tooltip_manager.h"

aegisub/src/dialog_style_editor.cpp
@@ -50,7 +50,6 @@
 #include "ass_override.h"
 #include "ass_style.h"
 #include "ass_style_storage.h"
-#include "colour_button.h"
 #include "dialog_colorpicker.h"
 #include "dialog_style_editor.h"
 #include "help_button.h"

aegisub/src/dialog_translation.h
@@ -31,8 +31,7 @@
 
 /// @file dialog_translation.h
 /// @see dialog_translation.cpp
-/// @ingroup tools_ui
-///
+/// @ingroup tools_ui///
 
 
 #ifndef DIALOG_TRANSLATION_H

aegisub/src/font_file_lister.h
@@ -35,9 +35,6 @@
 ///
 
 
-#pragma once
-
-
 ////////////
 // Includes
 #ifndef AGI_PRE

aegisub/src/gl_wrap.h
@@ -35,8 +35,6 @@
 ///
 
 
-#pragma once
-
 #ifdef __APPLE__
 #include <OpenGL/GL.h>
 #include <OpenGL/glu.h>

aegisub/src/subs_edit_box.cpp
@@ -58,7 +58,6 @@
 #include "main.h"
 #include "options.h"
 #include "subs_edit_box.h"
-#include "subs_edit_ctrl.h"
 #include "subs_grid.h"
 #include "timeedit_ctrl.h"
 #include "tooltip_manager.h"

commit 576e9603dd1bb70df3d00c4c14fe85f798835b9c

Make font_file_lister.cpp build on windows when freetype2 is disabled (broken by r3536).
git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@3609 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/font_file_lister.cpp
@@ -49,6 +49,8 @@
 #include "font_file_lister_freetype.h"
 /// DOCME
 #define FontListerClass FreetypeFontFileLister
+#else
+#include "font_file_lister.h"
 #endif // WITH_FREETYPE2
 #else
 #include "font_file_lister_fontconfig.h"
@@ -59,7 +61,6 @@
 #include "standard_paths.h"
 #include "text_file_reader.h"
 #include "text_file_writer.h"
-
 
 /// DOCME
 FontFileLister *FontFileLister::instance = NULL;