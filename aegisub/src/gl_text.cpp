tgoyne/aegisub

commit 2a856b32deec841dc63c0ebb0a42b34842f6c3e7

Make OpenGLTextTexture use the smallest texture that is large enough to fit the requested glyph rather than always using 256x256
git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@4250 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/gl_text.cpp
@@ -267,7 +267,7 @@ OpenGLTextGlyph OpenGLText::CreateGlyph(int n) {
 
 	// No texture could fit it, create a new one
 	if (!ok) {
-		textures.push_back(new OpenGLTextTexture(256,256));
+		textures.push_back(new OpenGLTextTexture(glyph.w,glyph.h));
 		textures.back()->TryToInsert(glyph);
 	}
 
@@ -285,8 +285,9 @@ OpenGLTextGlyph OpenGLText::CreateGlyph(int n) {
 OpenGLTextTexture::OpenGLTextTexture(int w,int h) {
 	// Properties
 	x = y = nextY = 0;
-	width = SmallestPowerOf2(w);
-	height = SmallestPowerOf2(h);
+	width = max(SmallestPowerOf2(w), 64);
+	height = max(SmallestPowerOf2(h), 64);
+	width = height = max(width, height);
 	tex = 0;
 
 	// Generate and bind

commit 2626e6d676c7bfb9ab0cef8391e49ea322c894aa

Fix build error introduced in r4250 when not using precompiled headers.
git-svn-id: svn+ssh://aegisub/var/svn/aegisub/trunk@4251 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/gl_text.cpp
@@ -42,6 +42,7 @@
 #ifndef AGI_PRE
 #include <wx/dcmemory.h>
 #include <wx/image.h>
+#include <algorithm>
 #endif
 
 #include "gl_text.h"
@@ -283,6 +284,7 @@ OpenGLTextGlyph OpenGLText::CreateGlyph(int n) {
 /// @param h 
 ///
 OpenGLTextTexture::OpenGLTextTexture(int w,int h) {
+	using std::max;
 	// Properties
 	x = y = nextY = 0;
 	width = max(SmallestPowerOf2(w), 64);

aegisub/src/video_out_gl.cpp
@@ -39,6 +39,7 @@
 #ifndef AGI_PRE
 #include <wx/log.h>
 #include <algorithm>
+#include <utility>
 #endif
 
 using std::min;