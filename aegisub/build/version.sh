615242328b47925eaa55114193406f364c5ec095
Thomas Goyne <tgoyne@gmail.com>
2012-08-25 18:46:51 -0700

    Use master's revision number

--- aegisub/build/version.sh
+++ aegisub/build/version.sh
@@ -24,7 +24,7 @@ fi
 last_svn_revision=6962
 last_svn_hash="16cd907fe7482cb54a7374cd28b8501f138116be"
 
-git_revision=$(expr $last_svn_revision + $(git log --pretty=oneline $last_svn_hash..HEAD 2>/dev/null | wc -l))
+git_revision=$(expr $last_svn_revision + $(git log --pretty=oneline $last_svn_hash..master 2>/dev/null | wc -l))
 git_version_str=$(git describe --exact-match 2> /dev/null)
 if test x$git_version_str != x; then
   git_version_str="${git_version_str##v}"
