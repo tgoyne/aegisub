db63bb274e9312d0ecb6327330525f60f0880916
Thomas Goyne <plorkyeran@aegisub.org>
2012-10-11 16:46:53 -0700

    Initial implementation of the split editbox

--- aegisub/src/subs_edit_box.h
+++ aegisub/src/subs_edit_box.h
@@ -85,33 +85,34 @@ class SubsEditBox : public wxPanel {
 	SubtitleSelection sel;
 
 	/// Are the buttons currently split into two lines?
-	bool splitLineMode;
+	bool button_bar_split;
 	/// Are the controls currently enabled?
-	bool controlState;
+	bool controls_enabled;
 
 	agi::Context *c;
 
 	agi::signal::Connection file_changed_slot;
 
 	// Box controls
-	wxCheckBox *CommentBox;
-	wxComboBox *StyleBox;
-	Placeholder<wxComboBox> *ActorBox;
-	TimeEdit *StartTime;
-	TimeEdit *EndTime;
-	TimeEdit *Duration;
-	wxSpinCtrl *Layer;
-	wxTextCtrl *MarginL;
-	wxTextCtrl *MarginR;
-	wxTextCtrl *MarginV;
-	Placeholder<wxComboBox> *Effect;
-	wxRadioButton *ByTime;
-	wxRadioButton *ByFrame;
+	wxCheckBox *comment_box;
+	wxComboBox *style_box;
+	Placeholder<wxComboBox> *actor_box;
+	TimeEdit *start_time;
+	TimeEdit *end_time;
+	TimeEdit *duration;
+	wxSpinCtrl *layer;
+	wxTextCtrl *margin_left;
+	wxTextCtrl *margin_right;
+	wxTextCtrl *margin_vertical;
+	Placeholder<wxComboBox> *effect_box;
+	wxRadioButton *by_time;
+	wxRadioButton *by_frame;
+	wxCheckBox *split_box;
 
-	wxSizer *TopSizer;
-	wxSizer *MiddleBotSizer;
-	wxSizer *MiddleSizer;
-	wxSizer *BottomSizer;
+	wxSizer *top_sizer;
+	wxSizer *middle_right_sizer;
+	wxSizer *middle_left_sizer;
+	wxSizer *bottom_sizer;
 
 	void SetControlsState(bool state);
 	/// @brief Update times of selected lines
@@ -122,26 +123,27 @@ class SubsEditBox : public wxPanel {
 	void CommitText(wxString const& desc);
 
 	/// Last commit ID for undo coalescing
-	int commitId;
+	int commit_id;
 
 	/// Last used commit message to avoid coalescing different types of changes
-	wxString lastCommitType;
+	wxString last_commit_type;
 
 	/// Last field to get a time commit, as they all have the same commit message
-	int lastTimeCommitType;
+	int last_time_commit_type;
 
 	/// Timer to stop coalescing changes after a break with no edits
-	wxTimer undoTimer;
+	wxTimer undo_timer;
 
 	/// The start and end times of the selected lines without changes made to
 	/// avoid negative durations, so that they can be restored if future changes
 	/// eliminate the negative durations
-	boost::container::map<AssDialogue *, std::pair<AssTime, AssTime> > initialTimes;
+	boost::container::map<AssDialogue *, std::pair<AssTime, AssTime>> initial_times;
 
 	// Constructor helpers
 	wxTextCtrl *MakeMarginCtrl(wxString const& tooltip, void (SubsEditBox::*handler)(wxCommandEvent&));
 	TimeEdit *MakeTimeCtrl(bool end, wxString const& tooltip, void (SubsEditBox::*handler)(wxCommandEvent&));
 	void MakeButton(const char *cmd_name);
+	wxButton *MakeBottomButton(const char *cmd_name);
 	wxComboBox *MakeComboBox(wxString const& initial_text, int style, void (SubsEditBox::*handler)(wxCommandEvent&), wxString const& tooltip);
 	wxRadioButton *MakeRadio(wxString const& text, bool start, wxString const& tooltip);
 
@@ -150,6 +152,7 @@ class SubsEditBox : public wxPanel {
 
 	void OnActiveLineChanged(AssDialogue *new_line);
 	void OnSelectedSetChanged(const SubtitleSelection &, const SubtitleSelection &);
+	void OnLineInitialTextChanged(wxString const& new_text);
 
 	void OnFrameTimeRadio(wxCommandEvent &event);
 	void OnStyleChange(wxCommandEvent &event);
@@ -165,6 +168,7 @@ class SubsEditBox : public wxPanel {
 	void OnEffectChange(wxCommandEvent &);
 	void OnSize(wxSizeEvent &event);
 	void OnUndoTimer(wxTimerEvent&);
+	void OnSplit(wxCommandEvent&);
 
 	void SetPlaceholderCtrl(wxControl *ctrl, wxString const& value);
 
@@ -196,10 +200,11 @@ class SubsEditBox : public wxPanel {
 	/// @brief Enable or disable frame timing mode
 	void UpdateFrameTiming(agi::vfr::Framerate const& fps);
 
-	/// Call a command the restore focus to the edit box
+	/// Call a command then restore focus to the edit box
 	void CallCommand(const char *cmd_name);
 
-	SubsTextEditCtrl *TextEdit;
+	SubsTextEditCtrl *edit_ctrl;
+	wxTextCtrl *secondary_editor;
 	agi::scoped_ptr<TextSelectionController> textSelectionController;
 
 public:
