13fb55c81401a1f426cab484469eb7d06e2a104a
Thomas Goyne <plorkyeran@aegisub.org>
2012-11-02 21:24:11 -0700

    Add a button to the split editbox to remove the line text only, leaving override tags

--- aegisub/src/ass_dialogue.h
+++ aegisub/src/ass_dialogue.h
@@ -85,6 +85,7 @@ public:
 
 class AssDialogueBlockPlain : public AssDialogueBlock {
 public:
+	using AssDialogueBlock::text;
 	AssBlockType GetType() const override { return BLOCK_PLAIN; }
 	AssDialogueBlockPlain(wxString const& text = wxString()) : AssDialogueBlock(text) { }
 };
