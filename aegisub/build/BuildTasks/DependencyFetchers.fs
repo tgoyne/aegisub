dcac451e12855cf1952b1bf0b3fbd92a94db7cb7
Thomas Goyne <plorkyeran@aegisub.org>
2013-04-21 14:42:31 -0700

    Remove most of the dependency fetching since it's covered by submodules now

--- aegisub/build/BuildTasks/DependencyFetchers.fs
+++ aegisub/build/BuildTasks/DependencyFetchers.fs
@@ -23,78 +23,6 @@ open Microsoft.Build.Evaluation
 open Microsoft.Build.Framework
 open Microsoft.Build.Utilities
 
-type GitWrapper(path : String) =
-  inherit ToolTask()
-
-  member val Arguments = "" with get, set
-  member val WorkingDirectory = "" with get, set
-
-  // ToolTask overrides
-  override val ToolName = "git.exe" with get
-  override this.GenerateFullPathToTool() = path
-  override this.GenerateCommandLineCommands() = this.Arguments
-  override this.GetWorkingDirectory() = this.WorkingDirectory
-
-  override this.Execute() =
-    if this.GenerateFullPathToTool() |> IO.File.Exists |> not then
-      failwith "git.exe not found. Make sure the MSYS root is set to a correct location."
-
-    this.UseCommandProcessor <- false
-    this.StandardOutputImportance <- "High"
-    base.Execute()
-
-type GitProject() =
-  inherit Task()
-
-  member val Projects : ITaskItem[] = null with get, set
-  member val Root = "" with get, set
-  member val GitPath = "" with get, set
-
-  override this.Execute() =
-    let callGit dir args =
-      let gw = GitWrapper(this.GitPath,
-                          BuildEngine = this.BuildEngine,
-                          HostObject = this.HostObject,
-                          Arguments = args,
-                          WorkingDirectory = dir)
-      if not <| gw.Execute() then failwith "git failed"
-
-    let update (projectName : String) directory url =
-      this.Log.LogMessage ("Updating {0}", projectName)
-      callGit directory "pull --rebase"
-      callGit directory "clean -xfd"
-
-    let fetch (projectName : String) root (url : ITaskItem) =
-      this.Log.LogMessage ("Fetching {0}", projectName)
-      ignore <| IO.Directory.CreateDirectory root
-
-      callGit root (sprintf "clone %s --no-checkout" url.ItemSpec)
-
-      let dir = (sprintf "%s\\%s" root projectName)
-
-      let autocrlf = url.GetMetadata "AutoCrlf"
-      if autocrlf.Length > 0
-      then callGit dir (sprintf "config --local core.autocrlf %s" autocrlf)
-
-      let branch = match url.GetMetadata("Branch") with "" -> "master" | x -> x
-      callGit dir (sprintf "checkout %s" branch)
-
-    let updateGit (url : ITaskItem) =
-      let projectName = Uri(url.ItemSpec).Segments.Last().Replace(".git", "")
-      let directory = sprintf "%s\\%s" this.Root projectName
-
-      if IO.Directory.Exists directory
-      then update projectName directory url
-      else fetch projectName this.Root url
-
-    this.Log.LogMessage ("Using git at {0}", this.GitPath)
-    try
-      this.Projects |> Array.map updateGit |> ignore
-      true
-    with e ->
-      this.Log.LogErrorFromException e
-      false
-
 let downloadArchive (url : String) unpackDest =
   use wc = new Net.WebClient()
   use downloadStream = wc.OpenRead url
@@ -102,45 +30,6 @@ let downloadArchive (url : String) unpackDest =
   use tarStream = new ICSharpCode.SharpZipLib.Tar.TarInputStream(gzStream)
   use tarArchive = ICSharpCode.SharpZipLib.Tar.TarArchive.CreateInputTarArchive tarStream
   tarArchive.ExtractContents unpackDest
-
-type TarballProject() =
-  inherit Task()
-
-  member val Projects : ITaskItem[] = null with get, set
-  member val Root = "" with get, set
-
-  override this.Execute() =
-    let needsUpdate directory version =
-      try
-        not <| String.Equals(sprintf "%s\\version.aegisub" directory |> IO.File.ReadAllText, version)
-      with | :? IO.IOException -> true
-
-    let update directory (project : ITaskItem) version =
-      try IO.Directory.Delete(directory, true) with | :? IO.IOException -> ()
-
-      this.Log.LogMessage ("Downloading {0} {1} from {2}", project.ItemSpec, version, project.GetMetadata "Url")
-      downloadArchive (project.GetMetadata "Url") (sprintf @"%s\.." directory)
-
-      let dirname = project.GetMetadata "DirName"
-      if not <| String.IsNullOrWhiteSpace dirname
-      then IO.Directory.Move(dirname |> sprintf @"%s\..\%s" directory, directory)
-
-      IO.File.WriteAllText(sprintf @"%s\version.aegisub" directory, version)
-
-    let check (project : ITaskItem) =
-      let directory = sprintf "%s\\%s" this.Root project.ItemSpec
-      let version = project.GetMetadata "Version"
-
-      if needsUpdate directory <| version
-      then update directory project version
-      else this.Log.LogMessage <| sprintf "%s is up to date" project.ItemSpec
-
-    try
-      this.Projects |> Array.map check |> ignore
-      true
-    with e ->
-      this.Log.LogErrorFromException e
-      false
 
 type DownloadTgzFile() =
   inherit Task()
